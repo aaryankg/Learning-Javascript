58b0a5838c993e64bec0f44d316bbab9ba3c2e52
Aaryan Kumar Gautam <126005333+aaryankg@users.noreply.github.com>
2024-04-24 12:32:38 +0000

    Variables in javascript

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 243234
+let accountEmail = "aaryankmrgtm@gmail.com"
+var accountPassword = "29484"
+accountCity = "Padrauna"
+
+
+// accountId = 984 not allowed
+console.log(accountEmail)
+
+accountEmail = "aaryan@gmail.com"
+accountPassword = "983484"
+accountCity = "Kushinagar"
+
+console.table([accountId, accountEmail, accountPassword, accountCity,])
