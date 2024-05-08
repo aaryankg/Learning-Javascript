81f934d215bf9f927f8ec38380d3710ab51211c4
Aaryan Kumar Gautam <126005333+aaryankg@users.noreply.github.com>
2024-05-08 13:10:44 +0000

    Consversion Operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+let score = "aaryan"
+
+// there are two ways to check type 
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// conversion 
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "Aaryan"
+
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 44
+
+let stringNumber = String (someNumber)
+
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
