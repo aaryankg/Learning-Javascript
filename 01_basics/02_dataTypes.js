31bdbe1ec51e67d969fe81785c1c7fd704f0acda
Aaryan Kumar Gautam <126005333+aaryankg@users.noreply.github.com>
2024-05-08 12:41:00 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+
+// alert(5+6) // we are using node js not browser
+console.log(8+0);
+console.log("aaryan kumar gautam");
+
+let name = "aaryan"
+let age = 21
+let isLoggedIn = true
+let state;
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone 
+// undefined => 
+// symbol => used for unique identification
+
+console.log(typeof undefined); // undefined 
+console.log(typeof null); // object is the type of null 
+
+
