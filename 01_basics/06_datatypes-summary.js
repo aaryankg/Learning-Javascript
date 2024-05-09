b5a04c3a366f0ad6a220662d2f4a966906c91f04
Aaryan Kumar Gautam <126005333+aaryankg@users.noreply.github.com>
2024-05-09 09:48:47 +0000

    data types summary

---
+++ 01_basics/06_datatypes-summary.js
@@ -0,0 +1,34 @@
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+// we use n at the end for bigInt
+
+// Reference (Non primitive)
+
+
+                              // Array, Objects, Functions
+
+// Array
+
+const heros = ["golu", "molu", "inshaan"];
+
+// Object
+
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+// Function
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// js is dynamicaly type language because it check the data type at runtime 
