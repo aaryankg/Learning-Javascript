80c25a93e75b5aba8a790b449cfe21e09a0909ee
Aaryan Kumar Gautam <aaryankmrgtm@gmail.com>
2024-05-15 04:30:20 +0000

    control flow

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,33 @@
+// Switch
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+// Suppose we do not use break. Then after the match below code will be also execute except default.
