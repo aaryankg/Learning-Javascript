b55f08f7569aa1cfe8b0542f10f0beadd9458112
Aaryan Kumar Gautam <aaryankmrgtm@gmail.com>
2024-05-16 11:32:12 +0000

    iterations

---
+++ 05_iterations/forOfAndMap.js
@@ -0,0 +1,43 @@
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    //console.log(num);
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    //console.log(`Each char is ${greet}`)
+}
+
+// Maps
+// The Map object holds key-value pairs and remembers the original insertion order of the keys.
+// A key in the Map may only occur once.
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    // console.log(key, ':-', value);
+    
+}
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+    
+// }
