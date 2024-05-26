1aa51db053c1eee0710375d9a2b696d3a323209a
Aaryan Kumar Gautam <aaryankmrgtm@gmail.com>
2024-05-26 10:29:06 +0000

    comparision

--- 01_basics/05_comparision.js
+++ 01_basics/05_comparision.js
@@ -23,6 +23,13 @@ console.log(undefined == 0);
 console.log(undefined > 0);
 console.log(undefined < 0);
 
+// In JavaScript, when comparing values using comparison operators like `<`, `>`, `<=`, and `>=`, the values being compared are converted to numbers before the comparison is made. When comparing a value like `undefined` to a number, the `undefined` value cannot be converted to a valid number, so it results in a special case where the comparison is not valid.
+
+// When you use `undefined < 0`, JavaScript tries to convert `undefined` to a number before comparing it to `0`. However, since `undefined` cannot be converted to a valid number, this operation results in `NaN` (Not-a-Number). Any comparison involving `NaN` will always return `false` because `NaN` is not less than, greater than, or equal to any other value, including `0`.
+
+// Therefore, `console.log(undefined < 0);` outputs `false` because the comparison involving `undefined` and `0` results in `NaN`, and any comparison with `NaN` will consistently return `false`.
+
+
 // === it also check datatypes
 
 console.log("2" === 2);
