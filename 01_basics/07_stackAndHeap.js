08c20c2e07f5a280e5d560b14c52634f68e752aa
Aaryan Kumar Gautam <aaryankmrgtm@gmail.com>
2024-05-26 10:55:29 +0000

    stack and heap

--- 01_basics/07_stackAndHeap.js
+++ 01_basics/07_stackAndHeap.js
@@ -3,6 +3,15 @@
 // heap (non primitive)
 // primitives values are stored in stack and non primitive in heap.
 
+// NOTE
+
+// In JavaScript, functions are indeed stored in the heap. When you define a function in JavaScript, it is stored as an object in memory, and objects in JavaScript are allocated memory in the heap.
+
+// When a function is declared, the function definition along with its lexical environment (variables, scopes, etc.) is stored on the heap. When the function is called, the function call information, parameters, and local variables are stored on the call stack.
+
+// So, while the function itself is stored in the heap, the call stack is used to keep track of the function calls and their execution.
+
+
 // example of primitive 
 
 let myName = "Aaryan Kumar Gautam"
